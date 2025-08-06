ed58ff851bf33b2426969c2eb87df2dff53b7fcc
Armmy <77948299+Vatcharapong09@users.noreply.github.com>
2025-08-06 08:27:50 +0000

    Pending changes exported from your codespace

--- public/script.js
+++ public/script.js
@@ -5,3 +5,30 @@
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
+
+//NEW
+const liffId = "2007882928-VWgW2jYN";
+const FromUrl = "https://obscure-space-spoon-7jx7qj44v7fx6q7-5173.app.github.dev/";
+
+async function reduirectUser() {
+  try {
+
+    await liff.init({liffId});
+
+    if(!liff.isLoggedIn()){
+      liff.login(window.location.href);
+      return;
+    }
+
+    const profile = await liff.getProfile();
+    const userId = profile.userId;
+    
+    window.location.replace("https://obscure-space-spoon-7jx7qj44v7fx6q7-5173.app.github.dev/");
+
+  } catch (error) {
+
+    console.error("LIFF ERROR : " + error);
+    alert("เกิดข้อผิดพลาด")
+
+  }
+}
